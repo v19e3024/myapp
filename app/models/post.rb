04e3679153f6bdf26eebd43a9814a07dd56e4020
keigo yunaga <ec2-user@ip-172-31-82-61.ec2.internal>
2020-07-19 03:24:51 +0000

    Lesson11課題

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@
 #  updated_at :datetime         not null
 #
 class Post < ApplicationRecord
+    has_many :comments
     validates :title, presence: true, length: { minimum: 3, message: 'Too short to post!' }
     validates :body, presence: true
 end
