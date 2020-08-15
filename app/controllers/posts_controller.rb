34b96c21990c7c1f461ea6e288afab81c44af999
keigo yunaga <ec2-user@ip-172-31-82-61.ec2.internal>
2020-08-15 11:35:40 +0000

    Lesson15の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,13 +10,15 @@ class PostsController < ApplicationController
     
     def new
         @post = Post.new
+        authorize @post
     end
     
     def create
         # render plain: params[:post].inspect
         # save
         # @post = Post.new(params.require(:post).permit(:title, :body))
-        @post = Post.new(post_params)
+        @post = Post.new(post_params.merge({user_id: session[:user_id]}))
+        authorize @post
         if @post.save
             # redirect
             redirect_to posts_path
@@ -28,10 +30,12 @@ class PostsController < ApplicationController
     
     def edit
         @post = Post.find(params[:id])
+        authorize @post
     end
     
     def update
         @post = Post.find(params[:id])
+        authorize @post
         if @post.update(post_params)
             redirect_to posts_path
         else
@@ -41,6 +45,7 @@ class PostsController < ApplicationController
     
     def destroy
         @post = Post.find(params[:id])
+        authorize @post
         @post.destroy
         redirect_to posts_path
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do |i|
-    Post.create(title: "title #{i}", body: "body #{i}")
+user = User.create(signin_name: "user1",display_name: "分大太郎",password: "bundai",password_confirmation: "bundai")
+
+3.times do |i|
+    user.posts.create(title: "title #{i}", body: "body #{i}")
 end
 
 3.times do |i|
